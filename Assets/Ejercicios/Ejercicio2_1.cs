895e13c33d0322342dbc9a841fe07a5f12107bf4
Martinawww03 <martinawang03@gmail.com>
2024-09-12 18:22:45 +0200

    Ejercicio2

---
+++ Assets/Ejercicios/Ejercicio2_1.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+    int edad;
+    float altura;
+    string nombre;
+    int iva;
+    float peso;
+    bool alumnosRepetidos= false;
+    char letra;
+    int minutos;
+    string matriculaCoche;
+    bool mayorDeEdad=false;
+    string codigoPostal;
+    bool genero=false;
+    int numeroDeHijo;
+    float tallaCamisa;
+    float precio;
+    string mensaje;
+    int mayorEdad;
+    int dias;
+    float contador;
+
+    // Start is called before the first frame update
+
+    void Start()
+    //2. EJERCICIOS DE MANEJO DE VARIABLES 
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
