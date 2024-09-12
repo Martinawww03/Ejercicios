478824d25fe0b1a55787080c89667c655f2ff219
Martinawww03 <martinawang03@gmail.com>
2024-09-12 18:04:39 +0200

    Update Ejercicio1_1.cs

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
+    string nombre = "Martina";
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hola Mundo!");
+        print("Este es el primer videojuego de "+ nombre);
+        print("Estoy aprendiendo C#");
+
+        Debug.Log("Ha pasado un frame ");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
 }
