f307b628b30f3ce9830a150eb4e091ffe1cbb052
Martinawww03 <martinawang03@gmail.com>
2024-09-16 12:17:57 +0200

    Ejercicio2_8

---
+++ Assets/Ejercicios/Ejercicio2_8.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_8 : MonoBehaviour
+{
+    int vidasPersonaje1 = 120;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
