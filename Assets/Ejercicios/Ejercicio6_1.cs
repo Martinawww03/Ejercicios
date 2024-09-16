0c346124cd5f8e63deddf5d3e496e9c2a0ec4152
Martinawww03 <martinawang03@gmail.com>
2024-09-16 19:54:03 +0200

    Ejercicio6

---
+++ Assets/Ejercicios/Ejercicio6_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //a_ T
+        //c_ T
+        //e_ T
+        //b_ T
+        //d_ F
+        //f_ T
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
