b8092a2f3072355770393f5ef0d7ec7d803ce0ca
Martinawww03 <martinawang03@gmail.com>
2024-09-16 11:21:12 +0200

    Update Ejercicio2_3.cs

--- Assets/Ejercicios/Ejercicio2_3.cs
+++ Assets/Ejercicios/Ejercicio2_3.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class Ejercicio2_3 : MonoBehaviour
 {
+    int vidas = 10;
+    float exp = 5.0f;
+    float producto;
+    float cociente;
+    float resto;
+    float doble;
+    float triple;
+
     // Start is called before the first frame update
     void Start()
     {
+        producto = vidas * exp;
+        cociente = vidas / exp;
+        resto = vidas / exp;
+        doble = vidas * 2;
+        triple = exp * 3;
         
     }
 
