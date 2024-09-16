2d709eed677284ac11a49b9c98d3018f317f1c3c
Martinawww03 <martinawang03@gmail.com>
2024-09-16 12:13:22 +0200

    Ejercicio2_7

---
+++ Assets/Ejercicios/Ejercicio2_7.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_7 : MonoBehaviour
+{
+    int oro = 7;
+    int segundos = 100;
+    int oroMinuto;
+    int oroHora;
+    int oroTotal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oroMinuto = oro * segundos;
+        oroHora = oroMinuto * 60;
+        oroTotal = oroHora * 4;
+        Debug.Log("el número total de  unidades de oro farmeadas en 4h = "+ oroTotal);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
