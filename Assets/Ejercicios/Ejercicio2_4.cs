5f1a85d21dca4bdb91ed3b78ffce4e19a4151c84
Martinawww03 <martinawang03@gmail.com>
2024-09-16 11:50:49 +0200

    Update Ejercicio2_4.cs

--- Assets/Ejercicios/Ejercicio2_4.cs
+++ Assets/Ejercicios/Ejercicio2_4.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Ejercicio2_4 : MonoBehaviour
 {
+    int vida = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vida += 77;
+        vida -= 3;
+        vida *= 4;
+        Debug.Log(vida);
     }
 
     // Update is called once per frame
