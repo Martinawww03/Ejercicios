3dc93d49152c0c4517f148fe7ce612c7334ef327
Martinawww03 <martinawang03@gmail.com>
2024-09-16 20:00:31 +0200

    Update Ejercicio6_1.cs

--- Assets/Ejercicios/Ejercicio6_1.cs
+++ Assets/Ejercicios/Ejercicio6_1.cs
@@ -7,12 +7,25 @@ public class Ejercicio6_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Ejercicio6.1
         //a_ T
         //c_ T
         //e_ T
         //b_ T
         //d_ F
         //f_ T
+
+        //Ejercicio6.2
+        //a_ T
+        //c_ F
+        //e_ F
+        //b_ F
+        //d_ T
+        //f_ F
+
+
+
+
     }
 
     // Update is called once per frame
