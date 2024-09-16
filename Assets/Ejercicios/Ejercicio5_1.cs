e584768b964d38ff7cecf7edff1a3c2f9f3bcb20
Martinawww03 <martinawang03@gmail.com>
2024-09-16 20:11:23 +0200

    ejercicio5_1

---
+++ Assets/Ejercicios/Ejercicio5_1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5_1 : MonoBehaviour
+{
+    int puntos1;
+    int puntos2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (puntos1==puntos1)
+        {
+            Debug.Log("");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
