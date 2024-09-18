1ac9dbb268a4ae5108be715dba7abc7ab286a8a6
Martinawww03 <martinawang03@gmail.com>
2024-09-18 18:15:07 +0200

    .

---
+++ Assets/Ejercicios/Ejercicio4_3.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio4_3 : MonoBehaviour
+{
+    float cantidad;
+    string dolar;
+    string euro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Convertir()
+    {
+        
+    }
+    
+}
