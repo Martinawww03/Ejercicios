00be899f4e4ce238188ae0d25fa79c4bc607a51c
Martinawww03 <martinawang03@gmail.com>
2024-09-18 16:26:14 +0200

    .

--- Assets/Ejercicios/Ejercicio7_3.cs
+++ Assets/Ejercicios/Ejercicio7_3.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio7_1 : MonoBehaviour
+public class Ejercicio7_3 : MonoBehaviour
 {
+    int numeros = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        while (numeros>0)
+        {
+            Debug.Log(numeros);
+            numeros--;
+        }
         
     }
 
