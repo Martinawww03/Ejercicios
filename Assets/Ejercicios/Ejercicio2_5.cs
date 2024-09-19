4a5fd5dce142ac0b69b5066acedb067aa4731a60
Martinawww03 <martinawang03@gmail.com>
2024-09-19 11:09:50 +0200

    Ejercicio2_5

--- Assets/Ejercicios/Ejercicio2_5.cs
+++ Assets/Ejercicios/Ejercicio2_5.cs
@@ -12,7 +12,10 @@ public class Ejercicio2_5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("vidasPlayer2= " + vidasPlayer3);
+        Debug.Log("vidasPlayer3= " + vidasPlayer1);
+        Debug.Log("vidasPlayer1= " + vidasPlayer4);
+        Debug.Log("vidasPlayer4= " + vidasPlayer2);
     }
 
     // Update is called once per frame
