bf6d7668991999f1aa85aac03228d46929ea38b4
Martinawww03 <martinawang03@gmail.com>
2024-09-19 23:35:49 +0200

    Ejercicio3_6

---
+++ Assets/Ejercicios/Ejercicio3_6.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio3_6 : MonoBehaviour
+{
+    [SerializeField] private float velocitykm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float VelocityMs(float velocityKm)
+    {
+        float velocityMs=velocityKm;
+        return velocityMs;
+
+    }
+}
