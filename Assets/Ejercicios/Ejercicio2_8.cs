f53e37e5b1ab36823de79de518f041dd9a9fc669
Martinawww03 <martinawang03@gmail.com>
2024-09-19 11:34:35 +0200

    Ejercicio2_8

--- Assets/Ejercicios/Ejercicio2_8.cs
+++ Assets/Ejercicios/Ejercicio2_8.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Ejercicio2_8 : MonoBehaviour
 {
-    int vidasPersonaje1 = 120;
+    int vidasTotal = 120;
+    int vidaActual;
+    int porcentajeVeneno = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        int dano = vidasTotal * (porcentajeVeneno / 100);
+        vidaActual -= dano;
+
+        Debug.Log("El personaje tiene " + vidaActual + " puntos de vida");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
