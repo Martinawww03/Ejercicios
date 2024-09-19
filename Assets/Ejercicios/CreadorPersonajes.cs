7e6b0dcee53b4c7ad9b76d9e9fd5c1dc870a956d
Martinawww03 <martinawang03@gmail.com>
2024-09-19 18:58:52 +0200

    .

---
+++ Assets/Ejercicios/CreadorPersonajes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorPersonajes : MonoBehaviour
+{
+    int numero = 5;
+    string texto = "Chalao";
+    float num = 3.4f;
+    [SerializeField]Personaje link;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/Ejercicios/Personaje.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    string nombre;
+    float vida;
+    float danho;
+    float velocidad;
+
+    //QUÉ puede hacer mi personaje
+    void Mover()
+    {
+
+    }
+    void Atacar()
+    {
+
+    }
+    void Saltar()
+    {
+
+    }
+
+}
