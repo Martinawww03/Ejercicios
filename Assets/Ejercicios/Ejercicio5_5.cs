fb716644fc62c2e835bc288918d5ab466e61163a
Martinawww03 <martinawang03@gmail.com>
2024-09-25 12:09:48 +0200

    Ejercicio5_5

---
+++ Assets/Ejercicios/Ejercicio5_5.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5_5 : MonoBehaviour
+{
+    [SerializeField] int nivel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
