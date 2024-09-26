33c9346a2935e060386cef7b8a1abe798b739493
Martinawww03 <martinawang03@gmail.com>
2024-09-26 10:48:53 +0200

    ejercicio5_7

--- Assets/Ejercicios/Ejercicio5_7.cs
+++ Assets/Ejercicios/Ejercicio5_7.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Ejercicio5_7 : MonoBehaviour
 {
+    int velocidadJugador1;
+    int velocidadJugador2;
+    int velocidadJugador3;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void ordenAtaque()
     {
-        
+        if(velocidadJugador1>=velocidadJugador2&&velocidadJugador1>=velocidadJugador3)
+        {
+            Debug.Log("Jugador 1 con velocidad: " + velocidadJugador1);
+            
+            if(velocidadJugador2>=velocidadJugador3)
+            {
+                Debug.Log("Jugador 2 con velocidad: " + velocidadJugador2);
+            }
+            
+        }
     }
+    
 }
