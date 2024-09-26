bcc609f439f1cacc67a9f9ee1bb067c1e40ea835
Martinawww03 <martinawang03@gmail.com>
2024-09-26 11:27:28 +0200

    Ejercicio5_10

--- Assets/Ejercicios/Ejercicio5_10.cs
+++ Assets/Ejercicios/Ejercicio5_10.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ejercicio5_10 : MonoBehaviour
 {
+    int temperatura;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    void clima()
+    {
+        if (temperatura <= 10)
+        {
+            Debug.Log("Tipo de clima es frio");
+        }
+       //else (10 < temperatura && temperatura <= 20) 
+       //{
+       //    Debug.Log("Tipo de clima es nublado");
+       //}
+       //else(20<temperatura&&temperatura<=30)
+       //{
+       //    Debug.Log("Tipo de clima es caluroso");
+       //}
+        else if(temperatura>30)
+        {
+            Debug.Log("Tipo de clima es tropical");
+        }
+    }
+    
+       
+
 }
